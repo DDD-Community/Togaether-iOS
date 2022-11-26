2a0c300105e2fea5c4b2c4301014a29ef58cae79
SangJin Han <s202188@naver.com>
2022-11-26 15:47:48 +0900

    chore: 스토리보드 삭제 및 폴더링 정리

---
+++ Together/App/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  Together
+//
+//  Created by 한상진 on 2022/11/26.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(
+        _ scene: UIScene, 
+        willConnectTo session: UISceneSession, 
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
