d3df176c3757dd50c5e05545b46f00f5efffa3d8
SangJin Han <s202188@naver.com>
2022-11-26 14:13:34 +0900

    feat: 기본 프로젝트 생성

---
+++ Together/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Together
+//
+//  Created by 한상진 on 2022/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
