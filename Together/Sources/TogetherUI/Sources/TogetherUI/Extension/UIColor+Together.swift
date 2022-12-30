0c538f999a5e5c4eb81e98ad59374e783c7e9fa5
SangJin Han <s202188@naver.com>
2022-12-31 00:40:19 +0900

    [UI] feat: 팔레트 값 추가

---
+++ Together/Sources/TogetherUI/Sources/TogetherUI/Extension/UIColor+Together.swift
@@ -0,0 +1,74 @@
+//
+//  File.swift
+//  
+//
+//  Created by 한상진 on 2022/12/31.
+//
+
+import UIKit
+
+// MARK: Primary Color
+
+public extension UIColor {
+    static var primary100: UIColor = hex("EDECFE")
+    static var primary200: UIColor = hex("DFDDFD")
+    static var primary300: UIColor = hex("C0BCFB")
+    static var primary400: UIColor = hex("9B94FF")
+    static var primary500: UIColor = hex("7A71FC")
+    static var primary600: UIColor = hex("6D65E0")
+    static var primary700: UIColor = hex("554FA5")
+    static var primary800: UIColor = hex("3B3776")
+    static var primary900: UIColor = hex("2A2753")
+}
+
+// MARK: Secondary Color
+
+public extension UIColor {
+    static var secondary100: UIColor = hex("D0F1E4")
+    static var secondary200: UIColor = hex("B6EDD7")
+    static var secondary300: UIColor = hex("91E1C1")
+    static var secondary400: UIColor = hex("73D4AD")
+    static var secondary500: UIColor = hex("6AC19E")
+    static var secondary600: UIColor = hex("6EAB93")
+    static var secondary700: UIColor = hex("5E927D")
+    static var secondary800: UIColor = hex("4B7464")
+    static var secondary900: UIColor = hex("2C443A")
+}
+
+// MARK: Blue Gray Color
+
+public extension UIColor {
+    static var blueGray100: UIColor = hex("F7F7F8")
+    static var blueGray150: UIColor = hex("EFEFF0")
+    static var blueGray200: UIColor = hex("E0E0E4")
+    static var blueGray300: UIColor = hex("C9C9CF")
+    static var blueGray400: UIColor = hex("9D9DA8")
+    static var blueGray500: UIColor = hex("80808E")
+    static var blueGray600: UIColor = hex("555566")
+    static var blueGray700: UIColor = hex("3B3B49")
+    static var blueGray800: UIColor = hex("282830")
+    static var blueGray900: UIColor = hex("0C0C0E")
+}
+
+// MARK: Background Color
+
+public extension UIColor {
+    static var backgroundWhite: UIColor = hex("FFFFFF")
+    static var backgroundGray: UIColor = hex("F9F9FD")
+    static var backgroundIvory100: UIColor = hex("FFFBF1")
+    static var backgroundIvory200: UIColor = hex("FFFBF2")
+}
+
+// MARK: Border Color
+
+public extension UIColor {
+    static var borderPrimary: UIColor = hex("E0E0E4")
+    static var borderSecondary: UIColor = hex("C9C9CF")
+}
+
+// MARK: States Color
+
+public extension UIColor {
+    static var error: UIColor = hex("F13C46")
+    static var success: UIColor = hex("8279F5")
+}
