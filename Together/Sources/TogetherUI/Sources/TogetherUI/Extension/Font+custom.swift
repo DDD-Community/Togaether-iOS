3646aef7af30bbd0123c7dbcec8ce147c2bb804e
SangJin Han <s202188@naver.com>
2022-12-31 23:14:14 +0900

    [UI] feat: Font extenstion 추가

---
+++ Together/Sources/TogetherUI/Sources/TogetherUI/Extension/Font+custom.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by 한상진 on 2022/12/31.
+//
+
+import UIKit
+
+public extension UIFont {
+    static var display5: UIFont? = .init(name: "Pretendard-Bold", size: 40)
+    static var display4: UIFont? = .init(name: "Pretendard-Bold", size: 36)
+    static var display3: UIFont? = .init(name: "Pretendard-Bold", size: 32)
+    static var display2: UIFont? = .init(name: "Pretendard-Bold", size: 28)
+    static var display1: UIFont? = .init(name: "Pretendard-Bold", size: 24)
+    static var headline: UIFont? = .init(name: "Pretendard-Bold", size: 20)
+    static var subhead3: UIFont? = .init(name: "Pretendard-Bold", size: 16)
+    static var subhead2: UIFont? = .init(name: "Pretendard-Bold", size: 14)
+    static var subhead1: UIFont? = .init(name: "Pretendard-Bold", size: 12)
+    
+    static var body2: UIFont? = .init(name: "Pretendard-Regular", size: 16)
+    static var body1: UIFont? = .init(name: "Pretendard-Regular", size: 14)
+    static var caption: UIFont? = .init(name: "Pretendard-Regular", size: 12)
+}
