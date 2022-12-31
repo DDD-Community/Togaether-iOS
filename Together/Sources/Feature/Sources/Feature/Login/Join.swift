fc1dcb1ff78016ac38ce527f9b2f9666ce76c6cc
SangJin Han <s202188@naver.com>
2023-01-01 01:50:07 +0900

    [회원가입] feat: UI 기본 구현

---
+++ Together/Sources/Feature/Sources/Feature/Login/Join.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  
+//
+//  Created by 한상진 on 2023/01/01.
+//
+
+import TogetherCore
+import ComposableArchitecture
+
+struct Join: ReducerProtocol {
+    struct State: Equatable {
+        
+    }
+    
+    enum Action: Equatable {
+        
+    }
+    
+    init() { }
+    
+    func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+        switch action {
+            
+        }
+    }
+    
+}
+
---
+++ Together/Sources/Feature/Sources/Feature/Login/JoinViewController.swift
@@ -0,0 +1,154 @@
+//
+//  File.swift
+//  
+//
+//  Created by 한상진 on 2023/01/01.
+//
+
+import UIKit
+import Combine
+
+import TogetherCore
+import TogetherUI
+import ThirdParty
+
+import SwiftLayout
+import ComposableArchitecture
+
+final class JoinViewController: UIViewController {
+    
+    private let store: StoreOf<Join>
+    private let viewStore: ViewStoreOf<Join>
+    private var cancellables: Set<AnyCancellable> = .init()
+    
+    private let scrollView: UIScrollView = .init()
+    
+    private let contentStackView: UIStackView = {
+        let stackView: UIStackView = .init()
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        return stackView
+    }()
+    
+    private let emailFieldView: TogetherInputFieldView = .init(title: "이메일(아이디)", placeholder: "예) example@togather.co.kr") 
+    private let passwordFieldView: TogetherInputFieldView = .init(title: "비밀번호", placeholder: "0자리 ~ 00자리의 영어, 숫자 혹은 특수문자")
+    private let passwordConfirmFieldView: TogetherInputFieldView = .init(title: "비밀번호 확인", placeholder: "비밀번호를 한 번 더 입력해주세요.") 
+    private let nameFieldView: TogetherInputFieldView = .init(title: "이름", placeholder: "00자 이내로 입력해주세요.") 
+    private let birthFieldView: TogetherInputFieldView = .init(title: "생년월일", placeholder: "yyyy - mm - dd") 
+    private let phoneNumberFieldView: TogetherInputFieldView = .init(title: "휴대폰 번호", placeholder: "예) 01012345678")
+    
+    private let confirmButton: TogetherRegularButton = .init(title: "회원가입 완료")
+    
+    
+    @LayoutBuilder var layout: some SwiftLayout.Layout {
+        confirmButton
+            .anchors { 
+                Anchors.height.equalTo(constant: 54)
+            }
+        
+        view
+            .config { view in
+                view.backgroundColor = .backgroundWhite
+            }
+            .sublayout {
+                contentStackView
+                    .config { stackView in
+                        stackView.addArrangedSubview(emailFieldView)
+                        stackView.setCustomSpacing(24, after: emailFieldView)
+                        stackView.addArrangedSubview(passwordFieldView)
+                        stackView.setCustomSpacing(24, after: passwordFieldView)
+                        stackView.addArrangedSubview(passwordConfirmFieldView)
+                        stackView.setCustomSpacing(24, after: passwordConfirmFieldView)
+                        stackView.addArrangedSubview(nameFieldView)
+                        stackView.setCustomSpacing(24, after: nameFieldView)
+                        stackView.addArrangedSubview(birthFieldView)
+                        stackView.setCustomSpacing(24, after: birthFieldView)
+                        stackView.addArrangedSubview(phoneNumberFieldView)
+                        stackView.setCustomSpacing(24, after: phoneNumberFieldView)
+                        
+                        stackView.addArrangedSubview(confirmButton)
+                    }
+                    .anchors { 
+                        Anchors.horizontal(offset: 24)
+                        Anchors.top.equalTo(view.safeAreaLayoutGuide)
+                    }
+            }
+    }
+    
+    init(store: StoreOf<Join>) {
+        self.store = store
+        self.viewStore = ViewStore(store)
+        super.init(nibName: nil, bundle: nil)
+        layout.finalActive()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindState()
+        bindAction()
+    }
+    
+    private func bindState() {
+    }
+    
+    private func bindAction() {
+        emailFieldView.inputTextField
+            .textPublisher
+            .sink { email in
+                print(email)
+            }
+            .store(in: &cancellables)
+        
+        passwordFieldView.inputTextField
+            .textPublisher
+            .sink { password in
+                print(password)
+            }
+            .store(in: &cancellables)
+        
+        passwordConfirmFieldView.inputTextField
+            .textPublisher
+            .sink { passwordConfirm in
+                print(passwordConfirm)
+            }
+            .store(in: &cancellables)
+        
+        nameFieldView.inputTextField
+            .textPublisher
+            .sink { name in
+                print(name)
+            }
+            .store(in: &cancellables)
+        
+        birthFieldView.inputTextField
+            .textPublisher
+            .sink { birth in
+                print(birth)
+            }
+            .store(in: &cancellables)
+        
+        phoneNumberFieldView.inputTextField
+            .textPublisher
+            .sink { phoneNumber in
+                print(phoneNumber)
+            }
+            .store(in: &cancellables)
+    }
+}
+
+#if DEBUG
+import SwiftUI
+
+struct JoinPreviews: PreviewProvider {
+    static var previews: some View {
+        let store: StoreOf<Join> = .init(initialState: .init(), reducer: Join())
+        return JoinViewController(store: store)
+            .showPrieview()
+    }
+}
+#endif
