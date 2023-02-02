e6bef08456777e5d1692eed616495996f9cc2fb8
SangJin Han <s202188@naver.com>
2023-02-02 20:16:54 +0900

    [네트워크] 에러 인터셉터 구현

--- Together/Sources/Core/TogetherCore/Sources/TogetherCore/NetworkClient/ErrorInterceptor.swift
+++ Together/Sources/Core/TogetherCore/Sources/TogetherCore/NetworkClient/ErrorInterceptor.swift
@@ -20,6 +20,42 @@ struct ErrorInterceptor: NetworkInterceptor {
         with error: Error, 
         options: NetworkRequestOptions
     ) async -> (URLRequest, RetryResult) {
+        let mappedError = map(error: error, response: response, data: data)
+        // sessionError일 경우 kickout 해야하지만 현재 미구현
         return (urlRequest, .doNotRetry(with: error))
     }
 }
+
+extension ErrorInterceptor {
+    private func map(error: Error, response: URLResponse?, data: Data?) -> Error {
+        if (error as NSError).code == NSURLErrorCancelled { return ResposeError.cancelled }
+        if let networkError = error as? NetworkError { return networkError }
+        guard let httpResponse = response as? HTTPURLResponse else { return ResposeError.unhandled(error: error) }
+        let statusCode = httpResponse.statusCode
+        // if let sessionError = SessionError(statusCode, error)
+        return ResposeError(statusCode: statusCode, error: error)
+    }
+}
+
+/// 현재는 사용할 일 x
+@frozen
+enum SessionError: Error {
+    case invalidSession
+    case invalidToken
+    case updateRequired
+}
+
+@frozen
+enum ResposeError: Error {
+    case cancelled
+    case unhandled(error: Error)
+    case invalidStatusCode(code: Int)
+    
+    init(statusCode: Int, error: Error) {
+        if statusCode == 500 {
+            self = .invalidStatusCode(code: statusCode)
+        } else {
+            self = .unhandled(error: error)
+        }
+    }
+} 
