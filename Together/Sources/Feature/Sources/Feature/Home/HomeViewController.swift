235ea6daf5385aa5a0583f822cd4d807439693e0
denny.k <denny.k@kakaocorp.com>
2023-02-09 00:33:01 +0900

    메인 api 호출 위치 변경

--- Together/Sources/Feature/Sources/Feature/Home/HomeViewController.swift
+++ Together/Sources/Feature/Sources/Feature/Home/HomeViewController.swift
@@ -132,7 +132,14 @@ final class HomeViewController: UIViewController, Layoutable {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        // MARK: Fetch Pet List
+        viewStore.send(.fetchPetList)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -159,9 +166,6 @@ final class HomeViewController: UIViewController, Layoutable {
                 self.guideAnimationView.fadeOut(duration: 0.5, completion: nil)
             }
         }
-
-        // MARK: Fetch Pet List
-        viewStore.send(.fetchPetList)
     }
 
     private func bindState() {
